ce89586ac026dd18585652dd3efb36b32f4a0712
shkherad <shkherad@bu.edu>
2016-07-31 22:41:09 -0400

    add image to recipes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160801023601) do
+ActiveRecord::Schema.define(version: 20160801023951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20160801023601) do
     t.integer  "time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
   create_table "schedules", force: :cascade do |t|
